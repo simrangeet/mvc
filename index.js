e1f2ec275de95f87800b2a3cd41559d3c3e58049
simrangeet <79188017+simrangeet@users.noreply.github.com>
2021-12-06 05:00:14 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    console.log("myname id ")
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrotation=30*htime+mtime/2;
+    mrotation=6*mtime;
+    srotation=6*stime;
+
+    hour.style.transform=`rotate(${hrotation}deg)`;
+    minutes.style.transform=`rotate(${mrotation}deg)`;
+    second.style.transform=`rotate(${srotation}deg)`;
+},1000);
